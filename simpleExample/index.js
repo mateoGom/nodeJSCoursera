db39b87a77502d19594fff7b51abf0c1e6906d49
mateoGom <matemgm@hotmail.com>
2020-10-28 16:13:28 +0100

    adding exports modules

--- simpleExample/index.js
+++ simpleExample/index.js
@@ -1,7 +1,4 @@
-const rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x,y) => (x * y)
-};
+var rect=require('./rectangle')
 
 const solveRect = (l, b) => {
   console.log("Ingresed parameters are l " + l + "  and b " + b);
