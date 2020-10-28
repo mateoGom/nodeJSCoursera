735d398f27937527a7db2efabe02f4838451c5cd
mateoGom <matemgm@hotmail.com>
2020-10-28 16:01:59 +0100

    simple example

---
+++ simpleExample/index.js
@@ -0,0 +1,16 @@
+const rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x,y) => (x * y)
+};
+
+const solveRect = (l, b) => {
+  console.log("Ingresed parameters are l " + l + "  and b " + b);
+  if (l <= 0 || b <= 0) {
+    console.log("Ingresed parameters must be highest than zero");
+  } else {
+    console.log("The perimeter of the body is =" + rect.perimeter(l, b));
+    console.log("The area of the body is =" + rect.area(l, b));
+  }
+};
+
+solveRect(0, 4);
