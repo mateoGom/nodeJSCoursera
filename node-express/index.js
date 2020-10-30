86012dcb4e350356d19e7b4d71b29864d187d43f
mateoGom <matemgm@hotmail.com>
2020-10-30 17:10:03 +0100

    using morgan

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
   console.log(req.headers);
@@ -14,10 +19,6 @@ app.use((req, res, next) => {
 });
 
 const server = http.createServer(app);
-server.listen(
-  port,
-  hostname,
-  () => {
-    console.log(`the server is running at http://${hostname}:${port}`);
-  });
-
+server.listen(port, hostname, () => {
+  console.log(`the server is running at http://${hostname}:${port}`);
+});
