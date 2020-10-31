66df5b5ac2e57d9d84aad8eb4654e7272e5ae42e
mateoGom <matemgm@hotmail.com>
2020-10-31 20:51:29 +0100

    adding a route

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const dishRoute= require('./routes/dishRoute');
 
 const hostname = "localhost";
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 const app = express();
 
 app.use(morgan("dev"));
+app.use('/dishes', dishRoute);
 
 app.use(express.static(__dirname + "/public"));
 
